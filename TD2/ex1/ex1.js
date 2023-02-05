a8038eb0ec87b7e0477a16e337d8b234b2e60fce
Aymene <aymanebagous04@gmail.com>
2023-02-05 15:26:11 +0100

    modified:   TD2/ex1/ex1.css 	modified:   TD2/ex1/ex1.js

--- TD2/ex1/ex1.js
+++ TD2/ex1/ex1.js
@@ -1,15 +1,38 @@
-$(function() {
-    $('li:contains("Sucre")').text('Miel');
-    $('li.hot').html(function() {
-        $('li.hot').hide();
-    })
-    $('hr.hot').html(function() {
-        $('hr.hot').hide();
-    })
+$(function() 
+{
+    $("li:contains('Sucre')").text('Miel');
 });
 
-$(function() {
+$(function()
+{
     $("li:eq(1)").css("font-style", "italic");
 });
 
-//3frf
+$(function()
+{
+    $("ul").append("<hr>");
+    $("ul").append("<li class='nothot' id='confiture'>Confiture</li>");
+});
+
+$(function()
+{
+    $("li:last-child, li:nth-last-child(3)").css("background-color", "#ff8827")
+});
+
+$(function()
+{
+    $("li").nextUntil("li:nth-last-child(3), li:last-child").prepend('+ ');
+});
+
+/*
+$(function()
+{
+    $("li:eq(1)").remove();
+    $("hr:eq(1)").remove();
+});
+*/
+
+$(function()
+{
+    
+});
